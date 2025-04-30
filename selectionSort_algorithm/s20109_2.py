60a61cb57a0c901fb58cd64ef7851292e2785b04
Bhanuka Janappriya <bhanukajanappriya2001@gmail.com>
2025-04-30 13:19:26 +0530

    Create basic structure of Selection sort algorithm

---
+++ selectionSort_algorithm/s20109_2.py
@@ -0,0 +1,10 @@
+def selectionSort(arr,size):
+    for i in range(size-1):
+        min_index = i
+        
+    for j in range(i+1,size):
+        if arr[j] < arr[min_index]:
+            min_index = j
+            
+    (arr[i],arr[min_index]) = (arr[min_index],arr[i])
+
